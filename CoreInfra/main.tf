fe867c17fc6e8d18106a518e573e23e5e6bb7a8a
delosric <delosric@yahoo.com>
2017-11-14 17:25:12 +0100

    Third commit

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -12,6 +12,7 @@ resource "aws_vpc" "crc_vpc" {
 
 resource "aws_subnet" "crc_subnet" {
   vpc_id            = "${aws_vpc.crc_vpc.id}"
+map_public_ip_on_launch = true
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
 
@@ -25,5 +26,17 @@ resource "aws_internet_gateway" "crc_gw" {
 
   tags {
     Name = "crc_gateway"
+  }
+}
+
+resource "aws_route_table" "crc_r" {
+  vpc_id = "${aws_vpc.crc_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.crc_gw.id}"
+  }
+
+  tags {
+    Name = "crc_route"
   }
 }
