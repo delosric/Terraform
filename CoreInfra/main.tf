a55a368c52f13a5e035c75230c1edc0be1729525
delosric <delosric@yahoo.com>
2017-11-14 16:10:13 +0100

    Second Commit

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+resource "aws_vpc" "crc_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name = "crc_vpc"
+  }
+}
+
+resource "aws_subnet" "crc_subnet" {
+  vpc_id            = "${aws_vpc.crc_vpc.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags {
+    Name = "crc_subnet"
+  }
+}
